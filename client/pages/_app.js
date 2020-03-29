21e0dc78f82d23e6242e70ff97e1b60e992826b0
iamn00b <thirafi.dide@gmail.com>
2020-03-29 21:03:52 +0700

    setup client

---
+++ client/pages/_app.js
@@ -0,0 +1,8 @@
+import 'semantic-ui-css/semantic.min.css';
+
+export default function PayPayPerformanceReviewSystemApp({
+  Component,
+  pageProps,
+}) {
+  return <Component {...pageProps} />;
+}
---
+++ client/pages/index.tsx
@@ -0,0 +1,7 @@
+import { Header } from 'semantic-ui-react';
+
+function HomePage() {
+  return <Header as="h1">Hello World!</Header>;
+}
+
+export default HomePage;
